0455bef6761f11055bb49b84f9109de2052edd53
Siddhesh Dabholkar <iamsiddhesh22@gmail.com>
2024-12-21 20:20:12 +0530

    add sUI for fetching shared list and also continue chat

--- src/app/playground/shared/[id]/page.tsx
+++ src/app/playground/shared/[id]/page.tsx
@@ -8,17 +8,28 @@ import ChatHeader from "../../components/ChatHeader";
 import { ChatType } from "@/types/Chat";
 import { MayBe } from "@/types/common";
 import axios from "@/lib/axios";
+import { useUser } from "@/context/userContext";
+import { ShareType } from "@/types/Share";
+import { toast } from "sonner";
 
 const SharedDetail = () => {
+  const { user } = useUser();
   const { id } = useParams<{ id: string }>();
   const router = useRouter();
   const footerRef = useRef<HTMLDivElement>(null);
   const [containerHeight, setContainerHeight] = useState("100%");
-  const [sharedData, setSharedData] = useState(null);
+  const [sharedData, setSharedData] = useState<MayBe<ShareType>>(null);
   const [chatData, setChatData] = useState<MayBe<ChatType>>(null);
   const [allMessages, setAllMessages] = useState<any[]>([]);
+  const [isCreating, setIsCreating] = useState(false);
 
   const [isLoading, setIsLoading] = useState(false);
+
+  const isUserChat = user?._id === sharedData?.createdBy;
+  console.log({
+    user,
+    sharedData,
+  });
 
   const getSharedById = async (id: string) => {
     try {
@@ -34,6 +45,28 @@ const SharedDetail = () => {
     } catch (error) {
       console.error("Something went wrong in getSharedById due to ", error);
       setIsLoading(false);
+    }
+  };
+
+  const handleContinueChat = async () => {
+    try {
+      setIsCreating(true);
+      const { data } = await axios.post(`/share/continue`, {
+        shareId: id,
+      });
+      if (data.isError) {
+        toast("Something went wrong in continuing the chat");
+      } else {
+        router.push(`/playground/detail/${data.data._id}`);
+      }
+      setIsCreating(false);
+    } catch (error) {
+      toast("Something went wrong in continuing the chat");
+      setIsCreating(false);
+      console.error(
+        "Something went wrong in handleContinueChat due to ",
+        error
+      );
     }
   };
 
@@ -75,7 +108,19 @@ const SharedDetail = () => {
           </div>
           <footer ref={footerRef} className="absolute bottom-0 w-full">
             <div className="flex flex-row w-full items-center justify-center">
-              <Button className="w-full rounded-2xl">Continue Chat</Button>
+              <Button
+                disabled={isCreating}
+                className="w-full rounded-2xl"
+                onClick={() => {
+                  if (isUserChat) {
+                    router.push(`/playground/detail/${sharedData?.chatId}`);
+                  } else {
+                    handleContinueChat();
+                  }
+                }}
+              >
+                {isUserChat ? "View" : "Continue"} Chat
+              </Button>
             </div>
             <p className="text-[12px] text-center mt-1">
               lawyrr may make mistakes. Please use with discretion.
