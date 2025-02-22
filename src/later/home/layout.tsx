ad88e4735f66d1741301775c4e0025a491cc88f8
Siddhesh <iamsiddhesh22@gmail.com>
2025-02-22 17:37:09 +0530

    fixed import error

--- src/later/home/layout.tsx
+++ src/later/home/layout.tsx
@@ -2,7 +2,7 @@ import { Footer } from "@/components/landing/Footer";
 import React from "react";
 import Navbar from "./components/Navbar";
 import AppContainer from "@/components/AppContainer";
-import { Footerdemo } from "@/components/landing/Footer";
+// import { Footerdemo } from "@/components/landing/Footer";
 
 type HomeLayoutProps = Readonly<{
   children: React.ReactNode;
@@ -14,7 +14,7 @@ const HomeLayout: React.FC<HomeLayoutProps> = ({ children }) => {
       <div className="min-h-screen flex flex-col bg-gray-100 dark:bg-black text-gray-600 dark:text-gray-300 ">
         <Navbar />
         <div className="mx-auto px-2 container relative">{children}</div>
-        <Footer className="px-0 sm:px-0 lg:px-0 py-12" />
+        {/* <Footer className="px-0 sm:px-0 lg:px-0 py-12" /> */}
       </div>
       {/* <AppContainer>{children}</AppContainer> */}
     </>
