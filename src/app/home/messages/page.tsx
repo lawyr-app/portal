91ee2e947d7f7c7c92b163c781cf0f94d405688f
Siddhesh <iamsiddhesh22@gmail.com>
2025-03-04 06:25:51 +0530

    added notificattion message

--- src/app/home/messages/page.tsx
+++ src/app/home/messages/page.tsx
@@ -2,8 +2,10 @@ import React from "react";
 
 const Messages = () => {
   return (
-    <div className="flex flex-row items-center justify-center bg-blue-800 w-full h-full">
-      lmao
+    <div className="flex flex-row items-center justify-center  w-full h-full">
+      <p className="text-[0.85rem]">
+        Select a conversation from the list to view its details.
+      </p>
     </div>
   );
 };
--- src/app/home/notification/page.tsx
+++ src/app/home/notification/page.tsx
@@ -1,7 +1,52 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Ellipsis } from "lucide-react";
 import React from "react";
 
 const Notification = () => {
-  return <div>Notification</div>;
+  return (
+    <div className="flex w-full flex-col">
+      {new Array(20).fill("").map((m, i) => (
+        <NotificationCard key={i} />
+      ))}
+    </div>
+  );
 };
 
 export default Notification;
+
+const NotificationCard = () => {
+  return (
+    <Card className="w-full flex flex-row p-3 mt-2 justify-between gap-2">
+      <Avatar>
+        <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+      <div className="w-10/12 text-left text-[14px]">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus animi
+        labore autem reiciendis odit modi quod, ipsum, voluptate ea saepe
+        dolorem aliquam necessitatibus. Nemo assumenda, consequuntur est
+        voluptate atque placeat. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit.
+      </div>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost">
+            <Ellipsis className="h-5 w-5" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56">
+          <DropdownMenuItem>Mark as viewed</DropdownMenuItem>
+          <DropdownMenuItem>Delete</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </Card>
+  );
+};
