9b86b7f8fd25721bf9739611bcbb89a6b769fd46
Siddhesh <iamsiddhesh22@gmail.com>
2025-11-26 12:57:48 +0530

    redirect from the auth page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,10 @@ import { Navbar } from "@/components/landing/Navbar";
 import CommingSoon from "@/components/CommingSoon";
 import Features from "@/components/landing/Features";
 import { Footer } from "@/components/landing/Footer";
+import { redirect } from "next/navigation";
 
 export default function Home() {
+  redirect("/auth/signin");
   return (
     <div className="min-h-screen flex flex-col bg-white dark:bg-black">
       <Navbar />
